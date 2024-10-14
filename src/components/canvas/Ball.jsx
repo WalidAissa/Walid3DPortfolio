0a62f47a90ee4aacc1c0845e2e00fe536e0744d0
Walid Aissa <walidaissa047@gmail.com>
2024-10-14 15:11:23 -0400

    floating balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -8,13 +8,13 @@ const Ball = (props) => {
 
   const [decal] = useTexture([props.imgUrl])
   return (
-    <Float speed={1.25} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25}/>
       <directionalLight position={[0,0,0.05]}/>
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1,1]}></icosahedronGeometry>
         <meshStandardMaterial
-        color="#fff8eb"
+        color="#ffffff"
         polygonOffset
         polygonOffsetFactor={-5}
         flatShading
@@ -22,7 +22,7 @@ const Ball = (props) => {
         <Decal 
         position={[0,0,1]}
         rotation={[2*Math.PI,0,6.25]}
-        floatShading
+        flatShading
         map = {decal}>
         </Decal>
       </mesh>
@@ -34,7 +34,6 @@ const Ball = (props) => {
 const BallCanvas = ({icon}) => {
   return(
     <Canvas
-    frameloop="demand"
     gl={{preserveDrawingBuffer: true}}
     >
       <Suspense fallback={<Loader/>}>
